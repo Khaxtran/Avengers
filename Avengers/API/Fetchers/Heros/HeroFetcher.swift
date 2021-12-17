66794951ea6b8dafa9042e4e042413088fb5ee7c
Kha Tran <khatran961217@gmail.com>
2021-12-17 14:49:14 +1100

    TD-1712
    Created generic API fetcher
    Created HeroModel

---
+++ Avengers/API/Fetchers/Heros/HeroFetcher.swift
@@ -0,0 +1,16 @@
+//
+//  HeroFetcher.swift
+//  Avengers
+//
+//  Created by Kha Tran on 17/12/21.
+//
+
+import Foundation
+
+class HeroFetcher: Codable {
+    
+    func fetchHero() {
+
+    }
+    
+}
--- Avengers/ViewController.swift
+++ Avengers/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let table: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(table)
+        table.delegate = self
+        table.dataSource = self
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell 
+    }
 
 }
 
